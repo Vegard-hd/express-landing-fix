238da804c8610b6256d06d519e0653f6cce08d30
vegard-hd <vegard.hd@outlook.com>
2025-03-12 18:44:17 +0100

    feat(api)!: backend gets alltime and 7days page stats and populates the cache with it

---
+++ middleware/middlewares.js
@@ -0,0 +1,109 @@
+const { LRUCache } = require("lru-cache");
+const options = {
+  max: 1,
+
+  // for use with tracking overall storage size
+
+  // how long to live in ms
+  ttl: 30000,
+
+  // return stale items before removing from cache?
+  allowStale: false,
+
+  updateAgeOnGet: false,
+  updateAgeOnHas: false,
+};
+
+const cache = new LRUCache(options);
+const axios = require("axios").default;
+const requestData = {
+  site_id: "vegardhaglund.dev",
+  metrics: ["visitors", "visits", "pageviews", "visit_duration"],
+  date_range: "all",
+  // filters: [["is_not", "visit:country_name", [""]]],
+  // dimensions: ["visit:country_name", "visit:city_name"],
+};
+
+const requestData7days = {
+  site_id: "vegardhaglund.dev",
+  metrics: ["visitors", "visits", "pageviews", "visit_duration"],
+  date_range: "7d",
+  // filters: [["is_not", "visit:country_name", [""]]],
+  // dimensions: ["visit:country_name", "visit:city_name"],
+};
+
+const key = process.env.PLAUSIBLE_KEY;
+
+async function getStats() {
+  // @ts-ignore
+  return await new Promise(async (resolve, reject) => {
+    const getApiData = async (queryOptions) => {
+      const { data } = await axios.post(
+        "https://required.vegardhaglund.dev/api/v2/query",
+        queryOptions,
+        {
+          headers: {
+            setContentType: "application/json",
+            Authorization: "Bearer " + key,
+          },
+        },
+      );
+      return await data;
+    };
+    // @ts-ignore
+    const [data, data2] = await Promise.all([
+      await getApiData(requestData),
+      await getApiData(requestData7days),
+    ]);
+    // @ts-ignore
+    console.log(data);
+    const newData = {
+      allTime: {
+        // @ts-ignore
+        visitors: data?.results[0]?.metrics[0] ?? 0,
+        // @ts-ignore
+
+        visits: data?.results[0]?.metrics[1] ?? 0,
+
+        // @ts-ignore
+        pageviews: data?.results[0]?.metrics[2] ?? 0,
+
+        // @ts-ignore
+        visitDuration: data?.results[0]?.metrics[3] ?? 0,
+      },
+      lastWeek: {
+        // @ts-ignore
+        visitors: data2?.results[0]?.metrics[0] ?? 0,
+        // @ts-ignore
+
+        visits: data2?.results[0]?.metrics[1] ?? 0,
+
+        // @ts-ignore
+        pageviews: data2?.results[0]?.metrics[2] ?? 0,
+
+        // @ts-ignore
+        visitDuration: data2?.results[0]?.metrics[3] ?? 0,
+      },
+    };
+    resolve(newData);
+  });
+}
+
+module.exports = {
+  // @ts-ignore
+  async isCached(req, res, next) {
+    try {
+      let stats = cache.get("latest");
+      console.log("iscached is ", stats);
+      if (!stats) {
+        stats = await getStats();
+        cache.set("latest", stats);
+        console.log("API data from cache used");
+      }
+      req.cache = stats;
+      next();
+    } catch (error) {
+      next(error);
+    }
+  },
+};
--- routes/index.js
+++ routes/index.js
@@ -1,33 +1,7 @@
 const express = require("express");
 const router = express.Router();
 require("dotenv").config();
-const axios = require("axios").default;
-const { LRUCache } = require("lru-cache");
-const options = {
-  max: 1,
-
-  // for use with tracking overall storage size
-
-  // how long to live in ms
-  ttl: 30000,
-
-  // return stale items before removing from cache?
-  allowStale: false,
-
-  updateAgeOnGet: false,
-  updateAgeOnHas: false,
-};
-
-const cache = new LRUCache(options);
-
-const requestData = {
-  site_id: "vegardhaglund.dev",
-  metrics: ["visitors", "visits", "pageviews", "visit_duration"],
-  date_range: "all",
-  // filters: [["is_not", "visit:country_name", [""]]],
-  // dimensions: ["visit:country_name", "visit:city_name"],
-};
-const key = process.env.PLAUSIBLE_KEY;
+const { isCached } = require("../middleware/middlewares");
 
 function formatSeconds(seconds = 10000) {
   let remaindingMinutes = 0;
@@ -43,40 +17,24 @@ function formatSeconds(seconds = 10000) {
   return `${hour}h ${minutes}m ${remainder}s`;
 }
 
-async function getStats() {
-  return await new Promise(async (resolve, reject) => {
-    const { data } = await axios.post(
-      "https://required.vegardhaglund.dev/api/v2/query",
-      requestData,
-      {
-        headers: {
-          setContentType: "application/json",
-          Authorization: "Bearer " + key,
-        },
-      },
-    );
-    const newData = {
-      visitors: data?.results[0]?.metrics[0] ?? 0,
-
-      visits: data?.results[0]?.metrics[1] ?? 0,
-
-      pageviews: data?.results[0]?.metrics[2] ?? 0,
-
-      visitDuration: data?.results[0]?.metrics[3] ?? 0,
-    };
-    resolve(newData);
-  });
-}
-
-router.get("/", async function (req, res, next) {
-  let stats = cache.get("latest");
-  console.log("iscached is ", stats);
-  if (!stats) {
-    stats = await getStats();
-    cache.set("latest", stats);
-    console.log("API data from cache used");
+router.get("/stats", isCached, async function (req, res, next) {
+  try {
+    // @ts-ignore
+    const stats = req.cache;
+    res.json(stats);
+  } catch (error) {
+    next(error);
   }
-  res.render("index", { stats: stats, formatSeconds: formatSeconds });
+});
+
+router.get("/", isCached, async function (req, res, next) {
+  try {
+    // @ts-ignore
+    const stats = req.cache;
+    res.render("index", { stats: stats, formatSeconds: formatSeconds });
+  } catch (error) {
+    next(error);
+  }
 });
 
 module.exports = router;
