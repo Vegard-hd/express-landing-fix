2bbfadc4c7a4283c0d4109aa5bdc13521a011859
vegard-hd <vegard.hd@outlook.com>
2025-04-11 21:01:52 +0200

    See previous!

--- middleware/middlewares.js
+++ middleware/middlewares.js
@@ -6,7 +6,7 @@ const options = {
   // for use with tracking overall storage size
 
   // how long to live in ms
-  ttl: 30000,
+  ttl: 1000 * 60 * 60 * 2,
 
   // return stale items before removing from cache?
   allowStale: false,
@@ -37,57 +37,63 @@ const key = process.env.PLAUSIBLE_KEY;
 
 async function getStats() {
   // @ts-ignore
-  return await new Promise(async (resolve, reject) => {
-    const getApiData = async (queryOptions) => {
-      const { data } = await axios.post(
-        "https://required.vegardhaglund.dev/api/v2/query",
-        queryOptions,
-        {
-          headers: {
-            setContentType: "application/json",
-            Authorization: "Bearer " + key,
+  try {
+    return await new Promise(async (resolve, reject) => {
+      const getApiData = async (queryOptions) => {
+        const { data } = await axios.post(
+          "https://required.vegardhaglund.website/api/v2/query",
+          queryOptions,
+          {
+            headers: {
+              setContentType: "application/json",
+              Authorization: "Bearer " + key,
+            },
           },
+        );
+        return await data;
+      };
+      // @ts-ignore
+      const [data, data2] = await Promise.all([
+        await getApiData(requestData),
+        await getApiData(requestData7days),
+      ]);
+      // @ts-ignore
+      data;
+      const newData = {
+        allTime: {
+          // @ts-ignore
+          visitors: data?.results[0]?.metrics[0] ?? 0,
+          // @ts-ignore
+
+          visits: data?.results[0]?.metrics[1] ?? 0,
+
+          // @ts-ignore
+          pageviews: data?.results[0]?.metrics[2] ?? 0,
+
+          // @ts-ignore
+          visitDuration: data?.results[0]?.metrics[3] ?? 0,
         },
-      );
-      return await data;
-    };
-    // @ts-ignore
-    const [data, data2] = await Promise.all([
-      await getApiData(requestData),
-      await getApiData(requestData7days),
-    ]);
-    // @ts-ignore
-    data;
-    const newData = {
-      allTime: {
-        // @ts-ignore
-        visitors: data?.results[0]?.metrics[0] ?? 0,
-        // @ts-ignore
+        lastWeek: {
+          // @ts-ignore
+          visitors: data2?.results[0]?.metrics[0] ?? 0,
+          // @ts-ignore
 
-        visits: data?.results[0]?.metrics[1] ?? 0,
+          visits: data2?.results[0]?.metrics[1] ?? 0,
 
-        // @ts-ignore
-        pageviews: data?.results[0]?.metrics[2] ?? 0,
+          // @ts-ignore
+          pageviews: data2?.results[0]?.metrics[2] ?? 0,
 
-        // @ts-ignore
-        visitDuration: data?.results[0]?.metrics[3] ?? 0,
-      },
-      lastWeek: {
-        // @ts-ignore
-        visitors: data2?.results[0]?.metrics[0] ?? 0,
-        // @ts-ignore
-
-        visits: data2?.results[0]?.metrics[1] ?? 0,
-
-        // @ts-ignore
-        pageviews: data2?.results[0]?.metrics[2] ?? 0,
-
-        // @ts-ignore
-        visitDuration: data2?.results[0]?.metrics[3] ?? 0,
-      },
-    };
-    resolve(newData);
-  });
+          // @ts-ignore
+          visitDuration: data2?.results[0]?.metrics[3] ?? 0,
+        },
+      };
+      resolve(newData);
+    });
+  } catch (error) {
+    console.error(
+      "Something wring with API request to plausible 'https://required.vegardhaglund.website/api/v2/query'",
+    );
+  }
 }
 
 module.exports = {
